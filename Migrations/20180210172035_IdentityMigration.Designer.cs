20d62c3412fbc8f9873b28c4c16ebade05ec0cd9
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-02-10 18:21:30 +0100

    migrations has been added

--- Migrations/20180210172035_IdentityMigration.Designer.cs
+++ Migrations/20180210172035_IdentityMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PieShopCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180210172035_IdentityMigration")]
+    partial class IdentityMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
